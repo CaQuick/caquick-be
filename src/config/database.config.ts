ac28b46fe7281fab155617f8364af6e944d3d7c3
chanwoo7 <chanwoo9987@gmail.com>
2025-12-21 04:02:05 +0900

    fix: 환경변수 누락 시 오류 처리 추가

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -13,6 +13,10 @@ export interface DatabaseConfig {
 export default registerAs('database', (): DatabaseConfig => {
   const url = process.env.DATABASE_URL ?? '';
 
+  if (!url) {
+    throw new Error('DATABASE_URL must be set');
+  }
+
   return {
     url,
   };
